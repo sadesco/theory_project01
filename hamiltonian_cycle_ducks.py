1bc958fe527c2385daa06957d1c8ddeb01df53c3
eleanor-f <eleanor.r.frederick@gmail.com>
2024-10-09 22:42:27 -0400

    initial functions

--- hamiltonian_cycle_ducks.py
+++ hamiltonian_cycle_ducks.py
@@ -1,1 +1,33 @@
 # Hamiltonian Cycle 
+
+import itertools
+import random
+import time
+import matplotlib.pyplot as plt
+
+def is_hamiltonian_cycle(graph, path):
+    """Check if a given path forms a Hamiltonian cycle in the graph"""
+    n = len(graph)
+    for i in range(n):
+        if graph[path[i]][path[(i + 1) % n]] == 0:
+            return False
+    return True
+
+def brute_force_hamiltonian(graph):
+    """Brute force solver for Hamiltonian cycle"""
+    n = len(graph)
+    vertices = list(range(n))
+    for perm in itertools.permutations(vertices[1:]):  # Fix first vertex
+        cycle = [vertices[0]] + list(perm)
+        if is_hamiltonian_cycle(graph, cycle):
+            return cycle
+    return None
+
+def time_hamiltonian(graph):
+    """Time the Hamiltonian cycle solver"""
+    start_time = time.time()
+    cycle = brute_force_hamiltonian(graph)
+    end_time = time.time()
+    elapsed_time = (end_time - start_time) * 1e6  # Time in microseconds
+    return cycle, elapsed_time
+
