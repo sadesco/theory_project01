303d96f902f60d023b24fcd0ae7f7cff85e05880
Molly Trickett <mtricket@nd.edu>
2024-10-12 19:10:29 -0400

    edits

--- hamiltonian_path_ducks.py
+++ hamiltonian_path_ducks.py
@@ -12,7 +12,7 @@ def is_hamiltonian_path(graph, path):
     return True
 
 #Finds all paths in a graph by generating all possible paths
-def hamilton_paths(graph, vertices):
+def hamiltonian_paths(graph, vertices):
     n = len(vertices)  
     paths = []  
 
@@ -30,19 +30,43 @@ def hamilton_paths(graph, vertices):
     generate(vertices, 0)
     return paths
 
+
+#generate random graphs to test
+def random(num_vertices, edge_probability=0.5):
+    graph = nx.gnp_random_graph(num_vertices, edge_probability)
+    matrix = nx.to_numpy_arry(graph, dtype=int)
+    return matrix
+
 #measures time it takes find all possible paths in a graph 
-def measure_time(func, *args):
+def measure_time(graph):
+    vertices = list(range(len(graph))) 
     start = time.time()
+    hamiltonian_paths(graph, vertices)
     end = time.time()
+
+    # return time taken 
     return end - start
 
-#plots all times measured 
-def plot_times(graphs):
-    sizes = []
+#plots performance - all times measured 
+def plot_times():
+    #sizes that represent the number of vertices 
+    sizes = [4, 5, 6, 7, 8, 9, 10] 
     times = []
-    for graph in graphs:
+    
+    for size in sizes: 
+        graph = random(size, edge_probability=0.5)
+        execution = measure_time(graph)
+        times.append(execution)
+        print("Size: {}, Execution time: {:.4f} seconds".format(size, execution))
         
-
+        plt.plot(sizes, times, marker='o')
+        plt.title("Hamiltonian Path Solver Performance")
+        plt.xlabel("Number of vertices")
+        plt.ylabel("Execution time (seconds)")
+        plt.show()
+        
+if __name__ == "__main__":
+    plot_times()
 
 
 
