f4de159871dce8e9913cf92303aa5b38fcc4527c
Molly Trickett <mtricket@nd.edu>
2024-10-13 19:29:22 -0400

    Done

--- hamiltonian_path_ducks.py
+++ hamiltonian_path_ducks.py
@@ -42,7 +42,7 @@ def hamiltonian_paths(graph, vertices):
 #generate random graphs to test
 def generate_random(num_vertices, edge_probability=0.5):
     graph = nx.gnp_random_graph(num_vertices, edge_probability)
-    matrix = nx.to_numpy_arry(graph, dtype=int)
+    matrix = nx.to_numpy_array(graph, dtype=int)
     return matrix
 
 def measure_time(graph):
