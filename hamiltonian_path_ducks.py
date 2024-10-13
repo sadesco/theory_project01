3d5f76206bf01725dd061539d013446f17ab0345
eleanor-f <eleanor.r.frederick@gmail.com>
2024-10-13 15:56:02 -0400

    added function descriptions

--- hamiltonian_path_ducks.py
+++ hamiltonian_path_ducks.py
@@ -3,20 +3,28 @@ import random
 import networkx as nx
 import matplotlib.pyplot as plt
 
-#Checks if path is an hamiltonian path
 def is_hamiltonian_path(graph, path):
+    """
+    This function takes a graph represented by an adjancency matrix and a proposed 
+    path and it checks if said path is a Hamiltonian path. 
+
+    """
     for i in range(len(path) - 1):
         #if no edge between two vertices return false
         if graph[path[i]][path[i+1]] == 0:
             return False
     return True
 
-#Finds all paths in a graph by generating all possible paths
 def hamiltonian_paths(graph, vertices):
+    """
+    Finds all hamiltonian paths in a graph by generating all possible 
+    paths. 
+    
+    """
     n = len(vertices)  
     paths = []  
 
-    #Generate all possible variations of the vertices
+    # generate all possible variations of the vertices
     def generate(path, index):
         if index == n:
             if is_hamiltonian_path(graph, path):
@@ -31,14 +39,20 @@ def hamiltonian_paths(graph, vertices):
     return paths
 
 
-#generate random graphs to test
 def random(num_vertices, edge_probability=0.5):
+    """
+    Generates random graphs to test 
+
+    """
     graph = nx.gnp_random_graph(num_vertices, edge_probability)
     matrix = nx.to_numpy_arry(graph, dtype=int)
     return matrix
 
-#measures time it takes find all possible paths in a graph 
 def measure_time(graph):
+    """
+    Measures the time it takes to generate all Hamiltonian paths in a graph
+    
+    """
     vertices = list(range(len(graph))) 
     start = time.time()
     hamiltonian_paths(graph, vertices)
@@ -47,8 +61,11 @@ def measure_time(graph):
     # return time taken 
     return end - start
 
-#plots performance - all times measured 
 def plot_times():
+    """
+    Plots and tests the performance of the solver
+    
+    """
     #sizes that represent the number of vertices 
     sizes = [4, 5, 6, 7, 8, 9, 10] 
     times = []
