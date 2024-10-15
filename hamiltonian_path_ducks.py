1eb819f7b61a2f2b16febf47c5a7f6245b075edb
Molly Trickett <mtricket@nd.edu>
2024-10-15 18:18:26 -0400

    Done

--- hamiltonian_path_ducks.py
+++ hamiltonian_path_ducks.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 
 def is_hamiltonian_path(graph, path):
     """
-    This function takes a graph represented by an adjancency matrix and a proposed 
-    path and it checks if said path is a Hamiltonian path. 
+    This function takes a graph represented by an adjancency matrix and a proposed
+    path and it checks if said path is a Hamiltonian path.
 
     """
     for i in range(len(path) - 1):
@@ -17,12 +17,12 @@ def is_hamiltonian_path(graph, path):
 
 def hamiltonian_paths(graph, vertices):
     """
-    Finds all hamiltonian paths in a graph by generating all possible 
-    paths. 
-    
+    Finds all hamiltonian paths in a graph by generating all possible
+    paths.
+
     """
-    n = len(vertices)  
-    paths = []  
+    n = len(vertices)
+    paths = []
 
     # generate all possible variations of the vertices
     def generate(path, index):
@@ -48,43 +48,60 @@ def generate_random(num_vertices, edge_probability=0.5):
 def measure_time(graph):
     """
     Measures the time it takes to generate all Hamiltonian paths in a graph
-    
+
     """
-    vertices = list(range(len(graph))) 
+    vertices = list(range(len(graph)))
     start = time.time()
     hamiltonian_paths(graph, vertices)
     end = time.time()
 
-    # return time taken 
+    # return time taken
     return end - start
 
 def plot_times():
     """
-    Plots and tests the performance of the solver
-    
+    Plots and tests the performance of the hamiltonian path solver
+
     """
-    #sizes that represent the number of vertices 
-    sizes = [4, 5, 6, 7, 8, 9, 10] 
+    sizes = [4, 5, 6, 7, 8, 9, 10] # sizes that represent the number of verticies
     avg_times = []
-    trials = 5 # number of trials
-    
-    for size in sizes: 
+    all_times = {}
+    trials = 5 # number of trials per graph size
+
+    for size in sizes:
         exec_times = []
         for _ in range(trials):
+            # generate a random graph with the current number of verticies
             graph = generate_random(size, edge_probability=0.5)
-            execution = measure_time(graph)
-            exec_times.append(execution)
-        
-        # take average
+            exec_time = measure_time(graph)
+            exec_times.append(exec_time)
+
+        # store all execution times for this given size
+        all_times[size] = exec_times
+
+        # take the average
         avg_time = np.mean(exec_times)
         avg_times.append(avg_time)
 
-        print("Size: {}, Execution time: {:.4f} seconds".format(size, avg_time))
-        
-    plt.plot(sizes, avg_times, marker='o')
-    plt.title("Hamiltonian Path Solver Performance")
+        # print the size of the graph and how long it took to execute
+        print(f"Size: {size}, Execution time: {avg_time:.4f} seconds")
+
+    # plot
+    plt.figure(figsize=(10, 6))
+
+    # scatter plot
+    for size in sizes:
+        jittered_size = [size + np.random.uniform(-0.1, 0.1) for _ in all_times[size]]
+        plt.scatter(jittered_size, all_times[size], color='blue', alpha=0.6, label='Execution times' if size == sizes[0] else "")
+
+    # plot average execution times
+    plt.plot(sizes, avg_times, marker='o', color='red', label='Average execution time')
+
+    plt.title("Hamiltonian Cycle Solver Performance")
     plt.xlabel("Number of vertices")
     plt.ylabel("Execution time (seconds)")
+    plt.legend()
+    plt.grid(True)
     plt.show()
 
 def test():
@@ -149,6 +166,3 @@ def test():
 if __name__ == "__main__":
     test()
     plot_times()
-
-
-
